52c7893c671f6442a638417b047a7a6e7597c58c
XX-05 <matt.r.aharonain@gmail.com>
2019-09-10 21:32:11 -0400

    created methods to refresh api access tokens and load cached info (SpotifyAPI._refresh_access_token(access_code) & SpotifyAPI._load_stored_info(data, name))

--- API/Spotify.py
+++ API/Spotify.py
@@ -33,6 +33,16 @@ class SpotifyAPI:
 
         self.token_info = None
 
+    def _load_stored_info(self, name):
+        file_name = base64.b64encode(name.encode("ascii"))
+        file_name = ".cache-%s" % file_name.decode("ascii")
+
+        if not os.path.isfile(file_name):
+            raise FileNotFoundError("cache file for '%s' not found!" % name)
+
+        with open(file_name, "r") as f:
+            return json.load(f)
+
     def _store_info(self, data, name):
         file_name = base64.b64encode(name.encode("ascii"))
         file_name = ".cache-%s" % file_name.decode("ascii")
@@ -112,25 +122,46 @@ class SpotifyAPI:
             raise SpotifyOAuthError("error requesting access token '%s'" % response.reason)
 
         token_info = response.json()
-        self.token_info = self._modify_token_expiration(token_info)
+        token_info = self._modify_token_expiration(token_info)
 
         self._refresh_auth_header(token_info)
         self._store_info(token_info, "SPOTIFY_TOKEN_INFO")
 
-        return self.token_info
+        self.token_info = token_info
+
+        return token_info
 
     def _refresh_access_token(self, token_info):
-        print("hi")
+        body = {
+            "grant_type": "refresh_token",
+            "refresh_token": self.token_info["refresh_token"]
+        }
+
+        header = self._make_auth_header(self.credentials["client_id"], self.credentials["client_secret"])
+        response = requests.post("https://accounts.spotify.com/api/token", body, headers=header)
+
+        if response.status_code != 200:
+            raise SpotifyOAuthError("error refreshing access token '%s'" % response.reason)
+
+        token_info = response.json()
+        token_info = self._modify_token_expiration(token_info)
+
+        self._refresh_auth_header(token_info)
+        self._store_info(token_info, "SPOTIFY_TOKEN_INFO")
+
+        self.token_info = token_info
+
+        return token_info
 
     def authenticate(self):
         base_url = "https://accounts.spotify.com/authorize"
 
-        creds = self.credentials.copy()
+        credentials = self.credentials.copy()
 
-        if creds["scope"] is None:
-            creds.pop("scope")
+        if credentials["scope"] is None:
+            credentials.pop("scope")
 
-        url = self._encode_uri(base_url, creds)
+        url = self._encode_uri(base_url, credentials)
         webbrowser.open_new_tab(url)
 
         parameters = self._listen_for_auth_code()
@@ -142,3 +173,5 @@ class SpotifyAPI:
             raise SpotifyOAuthError("error obtaining access code")
 
         token_info = self._request_access_token(parameters["code"])
+
+        return token_info
